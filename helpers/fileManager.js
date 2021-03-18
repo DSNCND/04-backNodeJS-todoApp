ae0a8f5dd078c647267f97f9f5244bee2611d3d8
Edson <edsoncondorimm@gmail.com>
2021-03-18 07:32:56 -0300

    Se creo la TODO app con dos items del menu. Crear tareas y Recuperar tareas

---
+++ helpers/fileManager.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const name = './database/data.json';
+
+const saveData = (data) =>
+{
+    fs.writeFileSync(name, JSON.stringify(data));
+}
+
+const getData = () =>
+{
+    if(!fs.existsSync(name))
+    {
+        return null;
+    }
+
+    return JSON.parse(fs.readFileSync(name, {encoding: 'utf8'}));
+
+}
+
+module.exports = 
+{
+    saveData,
+    getData,
+}
+
